ef899fcab50262966027747d84a8bbb19634c462
Florian Schweikert <kelvan@ist-total.org>
2015-03-18 14:45:57 +0100

    Add test

---
+++ test/test_input.py
@@ -0,0 +1,30 @@
+import unittest
+import tempfile
+from datetime import date
+
+from input import convert_dict, load_from_csv
+
+
+class TestCSVInput(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_load_valid_csv(self):
+        with tempfile.NamedTemporaryFile() as f:
+            f.write(b'date;project;hours;note\n'
+                    b'2000-01-01;Testproject;1.0;test note 1\n'
+                    b'2000-01-02;Testproject;2.5;test note 2\n')
+            f.seek(0)
+
+            lst = load_from_csv(f.name)
+            self.assertEqual(2, len(lst))
+            for i, l in enumerate(lst):
+                self.assertIn('date', l)
+                self.assertEqual(date(2000, 1, i+1), l['date'])
+                self.assertIn('project', l)
+                self.assertEqual('Testproject', l['project'])
+                self.assertIn('hours', l)
+                hours = 1 + i + i * 0.5
+                self.assertEqual(hours, l['hours'])
+                self.assertIn('note', l)
+                self.assertEqual('test note %d' % (i+1), l['note'])
